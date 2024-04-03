fb4742cde68a197679270b6b249219d47dbcf11c
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2024-04-02 21:08:06 -0300

    Add lilypond-mode to emacs config

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -11,5 +11,11 @@
   home.file = {
     ".emacs.d/early-init.el".source = ./emacs/early-init.el;
     ".emacs.d/init.el".source = ./emacs/init.el;
+    ".emacs.d/site-lisp/lilypond-font-lock.el".source = ./emacs/site-lisp/lilypond-font-lock.el;
+    ".emacs.d/site-lisp/lilypond-indent.el".source = ./emacs/site-lisp/lilypond-indent.el;
+    ".emacs.d/site-lisp/lilypond-init.el".source = ./emacs/site-lisp/lilypond-init.el;
+    ".emacs.d/site-lisp/lilypond-mode.el".source = ./emacs/site-lisp/lilypond-mode.el;
+    ".emacs.d/site-lisp/lilypond-what-beat.el".source = ./emacs/site-lisp/lilypond-what-beat.el;
+    ".emacs.d/site-lisp/lilypond-words.el".source = ./emacs/site-lisp/lilypond-words.el;
   };
 }
