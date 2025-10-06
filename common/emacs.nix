9de27bbee86d3d1c269c9d0cf600d7a63c4fab97
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2025-10-06 16:40:41 -0300

    Small improvements on emacs nix config

--- common/emacs.nix
+++ common/emacs.nix
@@ -54,11 +54,7 @@ in
   defaultInitFile = true;
 
   # Package is optional, defaults to pkgs.emacs
-  # TODO: remove "withNativeCompilation = false"
-  # package = pkgs.emacs-unstable;
-  package = pkgs.emacs-unstable.override {
-    withNativeCompilation = false;
-  };
+  package = pkgs.emacs-unstable;
 
   # By default emacsWithPackagesFromUsePackage will only pull in
   # packages with `:ensure`, `:ensure t` or `:ensure <package name>`.
