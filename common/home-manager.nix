7702882df7e624e0217871292d69968ddf79c03b
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2026-03-03 13:01:47 -0300

    Add zelda sounds

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -29,6 +29,16 @@
 
   # TODO: find out a cleaner way to do that
   home.file = {
+    # zelda sounds
+    ".local/share/sounds/zelda/cucco.wav".source = ./sounds/zelda/cucco.wav;
+    ".local/share/sounds/zelda/crystal.wav".source = ./sounds/zelda/crystal.wav;
+    ".local/share/sounds/zelda/fairy.wav".source = ./sounds/zelda/fairy.wav;
+    ".local/share/sounds/zelda/flute-1.wav".source = ./sounds/zelda/flute-1.wav;
+    ".local/share/sounds/zelda/low-hp.wav".source = ./sounds/zelda/low-hp.wav;
+    ".local/share/sounds/zelda/link-falls.wav".source = ./sounds/zelda/link-falls.wav;
+    ".local/share/sounds/zelda/secret.wav".source = ./sounds/zelda/secret.wav;
+    ".local/share/sounds/zelda/heart-container-1.wav".source = ./sounds/zelda/heart-container-1.wav;
+    ".local/share/sounds/zelda/item-get-1.wav".source = ./sounds/zelda/item-get-1.wav;
     # emacs
     ".emacs.d/early-init.el".source = ./emacs/early-init.el;
     ".emacs.d/site-lisp/lilypond-font-lock.el".source = ./emacs/site-lisp/lilypond-font-lock.el;
@@ -50,6 +60,7 @@
 
   home.shellAliases = {
     start-fluidsynth = "fluidsynth -a coreaudio -m coremidi ${pkgs.soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2";
+    claude = "echo 'Use claude-personal or claude-bluecode instead'";
     claude-personal = "CLAUDE_CONFIG_DIR=$HOME/.claude-personal command claude";
     claude-bluecode = "CLAUDE_CONFIG_DIR=$HOME/.claude-bluecode command claude";
   };
